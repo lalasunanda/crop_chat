d5662af4d995ec0b09e5001f4451494e1663f879
lalasunanda <lalasunandabai17@gmail.com>
2025-03-19 18:24:46 +0530

    Added settings

--- Agriculture/settings.py
+++ Agriculture/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = 'p_cvnmw4-*-&rm7n^gbp__+q8tcruq=t7j9!v&vd&rrajvp5zt'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['crop-yeild.onrender.com', 'localhost']
+ALLOWED_HOSTS = ['crop-chat.onrender.com', '127.0.0.1', 'localhost']
+
 
 
 
